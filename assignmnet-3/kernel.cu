f7093d1003f123b24ce4c5f78d5b3cfce853d473
Mayssa <mhh77@mail.aub.edu>
2026-02-05 14:16:03 +0200

    Add assignment 3

---
+++ assignmnet-3/kernel.cu
@@ -0,0 +1,121 @@
+
+#include "common.h"
+#include "timer.h"
+
+#define TILE_DIM 32
+
+__global__ void mm_tiled_kernel(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K) {
+
+    // TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void mm_gpu(float* A, float* B, float* C, unsigned int M, unsigned int N, unsigned int K) {
+
+    float elapsedTime;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // Allocate GPU memory
+    cudaEventRecord(start);
+
+    // TODO
+
+
+
+
+
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Allocation time: %.3f ms\n", elapsedTime);
+
+    // Copy data to GPU
+    cudaEventRecord(start);
+
+    // TODO
+
+
+
+
+
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Copy to GPU time: %.3f ms\n", elapsedTime);
+
+    // Call kernel
+    cudaEventRecord(start);
+
+    // TODO
+
+
+
+
+
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("\033[1;32mKernel time: %.3f ms\033[0m\n", elapsedTime);
+
+    // Copy data from GPU
+    cudaEventRecord(start);
+
+    // TODO
+
+
+
+
+
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Copy from GPU time: %.3f ms\n", elapsedTime);
+
+    // Free GPU memory
+    cudaEventRecord(start);
+
+    // TODO
+
+
+
+
+
+
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Deallocation time: %.3f ms\n", elapsedTime);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+}
+
