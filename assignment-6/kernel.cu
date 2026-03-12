7dbd420968b3612f805f6b0bdfc59a93cae89290
Mayssa <mhh77@mail.aub.edu>
2026-03-12 17:14:09 +0200

    Add assignment 6

---
+++ assignment-6/kernel.cu
@@ -0,0 +1,86 @@
+
+#include "common.h"
+#include "timer.h"
+
+#include <cuda/atomic>
+
+#define BLOCK_DIM 1024
+
+__global__ void reduce_kernel(float* input, float* sum, unsigned int N) {
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+float reduce_gpu(float* input, unsigned int N) {
+
+    float elapsedTime;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // Allocate memory
+    cudaEventRecord(start);
+    float *input_d;
+    cudaMalloc((void**) &input_d, N*sizeof(float));
+    float *sum_d;
+    cudaMalloc((void**) &sum_d, sizeof(float));
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Allocation time: %.3f ms\n", elapsedTime);
+
+    // Copy data to GPU
+    cudaEventRecord(start);
+    cudaMemcpy(input_d, input, N*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemset(sum_d, 0, sizeof(float));
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Copy to GPU time: %.3f ms\n", elapsedTime);
+
+    // Call kernel
+    cudaEventRecord(start);
+    const unsigned int numThreadsPerBlock = BLOCK_DIM;
+    const unsigned int numElementsPerBlock = 2*numThreadsPerBlock;
+    const unsigned int numBlocks = (N + numElementsPerBlock - 1)/numElementsPerBlock;
+    reduce_kernel <<< numBlocks, numThreadsPerBlock >>> (input_d, sum_d, N);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("\033[1;32mKernel time: %.3f ms\033[0m\n", elapsedTime);
+
+    // Copy data from GPU
+    cudaEventRecord(start);
+    float sum;
+    cudaMemcpy(&sum, sum_d, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Copy from GPU time: %.3f ms\n", elapsedTime);
+
+    // Free memory
+    cudaEventRecord(start);
+    cudaFree(input_d);
+    cudaFree(sum_d);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    printf("Deallocation time: %.3f ms\n", elapsedTime);
+
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+
+    return sum;
+
+}
+
